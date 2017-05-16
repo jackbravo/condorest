e533e8ec532e5c867a251bfae966c9d374e5d940
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-05-16 17:55:36 -0500

    Set dev database

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :condorest, Condorest.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "condorest_dev",
+  username: "condorest",
+  password: "condorest",
+  database: "condorest",
   hostname: "localhost",
   pool_size: 10
