3142024a30f2043a05fe8458deb23c84a807d824
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-06-01 15:26:33 -0500

    Do a left sidebar navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,9 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+$(document).ready(function() {
+  $('[data-toggle=collapse]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
