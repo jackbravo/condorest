da5af22318f16dcd3d853b6c1997d04bc122cdf7
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-07-11 19:31:00 -0500

    Validate start and end date in fee_lines

--- lib/condorest/revenue/fee_line.ex
+++ lib/condorest/revenue/fee_line.ex
@@ -19,6 +19,18 @@ defmodule Condorest.Revenue.FeeLine do
     fee_line
     |> cast(attrs, [:amount, :date_start, :date_end, :lot_id])
     |> validate_required([:amount, :date_start, :date_end, :lot_id])
+    |> validate_dates()
     |> foreign_key_constraint(:lot_id)
+  end
+
+  def validate_dates(changeset) do
+    date_start = Ecto.Changeset.get_field(changeset, :date_start)
+    date_end = Ecto.Changeset.get_field(changeset, :date_end)
+
+    if (date_start && date_end && Date.compare(date_start, date_end) == :gt) do
+      add_error(changeset, :date_end, "Date end must be later than date start")
+    else
+      changeset
+    end
   end
 end
